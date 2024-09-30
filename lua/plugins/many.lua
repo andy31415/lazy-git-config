0d6637ae3a088a0ed79a4ae378cd5eb537ddd54e
Andrei Litvin <andy314@gmail.com>
2024-09-29 22:17:09 -0400

    Some local updates

---
+++ lua/plugins/many.lua
@@ -0,0 +1,15 @@
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  {
+    "folke/trouble.nvim",
+    -- opts will be merged with the parent spec
+    opts = { use_diagnostic_signs = true },
+  },
+  -- Monokai still looks better to me
+  -- {
+  --   "catppuccin/vim",
+  -- },
+}
