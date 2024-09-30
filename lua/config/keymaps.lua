348011d2faffb056f43f7e1686c4b9116af9a832
Andrei Litvin <andy314@gmail.com>
2024-09-29 22:07:33 -0400

    First configs

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Press `-` to edit directory using oil
+map("n", "-", "<CMD>Oil<CR>")
+
+-- Move visual selection up/down with CTRL + j/k
+map("v", "<C-j>", ":m '>+1<CR>gv-gv")
+map("v", "<C-k>", ":m '<lt>-2<CR>gv-gv")
+
+map("n", "<Leader>or", "<CMD>OverseerRestartLast<CR>")
+map("n", "<Leader>of", "<CMD>ClangdSwitchSourceHeader<CR>")
