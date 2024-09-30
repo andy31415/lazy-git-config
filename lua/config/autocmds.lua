133975261fce2b49a7e90f0997638f5e1f111d72
Andrei Litvin <andreilitvin@google.com>
2024-09-30 14:52:07 -0400

    Do not auto format shell ... it is not what chip wants

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("no_auto_fmt"),
+  pattern = {
+    "sh",
+  },
+  callback = function(_)
+    vim.b.autoformat = false
+  end,
+})
