1e836ef48909349ed85141ead16fa9ed17479a13
Andrei Litvin <andy314@gmail.com>
2024-12-11 23:16:03 -0500

    Better multigrep, only preview is broken

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,28 +15,73 @@ map("n", "<Leader>or", "<CMD>OverseerRestartLast<CR>")
 map("n", "<Leader>of", "<CMD>ClangdSwitchSourceHeader<CR>")
 
 map("n", "<Leader>*", "<CMD>FzfLua grep_cword<CR>")
+
+-- Taken from `Advanced telescope.nvim` Examples, search plugin
+-- TODO: move this somewhere else
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
+local live_multigrep = function(opts)
+  opts = opts or {}
+  opts.cwd = opts.cwd or vim.uv.cwd()
+
+  local finder = finders.new_async_job({
+    command_generator = function(prompt)
+      if not prompt or prompt == "" then
+        return nil
+      end
+
+      local pieces = vim.split(prompt, "  ")
+      local args = {
+        "rg",
+        -- options required by ripgrep
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        -- options for my own search
+        "--hidden",
+        "--no-ignore",
+        "--max-columns=4096",
+        "-g",
+        "!out",
+        "-g",
+        "!.git",
+      }
+
+      if pieces[1] then
+        table.insert(args, "-e")
+        table.insert(args, pieces[1])
+      end
+
+      if pieces[2] then
+        table.insert(args, "-g")
+        table.insert(args, pieces[2])
+      end
+
+      return args
+    end,
+    entry_marker = make_entry.gen_from_vimgrep(opts),
+    cwd = opts.cwd,
+  })
+
+  pickers
+    .new(opts, {
+      debounce = 100,
+      prompt_title = "MultiGrep (double space for path glob)",
+      finder = finder,
+      previewer = conf.grep_previewer(opts),
+      sorter = require("telescope.sorters").empty(),
+    })
+    :find()
+end
+
 -- map("n", "<Leader>/", "<CMD>FzfLua live_grep_glob<cr>")
 map("n", "<Leader>/", function()
-  require("telescope.builtin").live_grep({
-    vimgrep_arguments = {
-      "rg",
-      -- options required by ripgrep
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-      -- options for my own search
-      "--hidden",
-      "--no-ignore",
-      "--max-columns=4096",
-      "-g",
-      "!out",
-      "-g",
-      "!.git",
-    },
-  })
+  live_multigrep()
 end, { desc = "RipGrep" })
 
 -- I am used to lunar vim key bindings and also
@@ -62,7 +107,7 @@ map({ "n" }, "<Leader>ci", function()
   print(vim.inspect(cmp))
 end, { desc = "Ollama complete" })
 
--- Taken from `Advanced telescope.nvim` Examples, searhc plugins
+-- Taken from `Advanced telescope.nvim` Examples, search plugin
 map({ "n" }, "<Leader>fp", function()
   require("telescope.builtin").find_files({
     cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
