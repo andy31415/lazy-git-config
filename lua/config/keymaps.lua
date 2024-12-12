f6e995bf870bd7fd1604b0f582ad14bb364e4247
Andrei Litvin <andy314@gmail.com>
2024-12-11 22:55:48 -0500

    Ripgrep that is customized for now

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,11 +15,40 @@ map("n", "<Leader>or", "<CMD>OverseerRestartLast<CR>")
 map("n", "<Leader>of", "<CMD>ClangdSwitchSourceHeader<CR>")
 
 map("n", "<Leader>*", "<CMD>FzfLua grep_cword<CR>")
-map("n", "<Leader>/", "<CMD>FzfLua live_grep_glob<cr>")
+-- map("n", "<Leader>/", "<CMD>FzfLua live_grep_glob<cr>")
+map("n", "<Leader>/", function()
+  require("telescope.builtin").live_grep({
+    vimgrep_arguments = {
+      "rg",
+      -- options required by ripgrep
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      -- options for my own search
+      "--hidden",
+      "--no-ignore",
+      "--max-columns=4096",
+      "-g",
+      "!out",
+      "-g",
+      "!.git",
+    },
+  })
+end, { desc = "RipGrep" })
 
 -- I am used to lunar vim key bindings and also
 -- I probably do not need the lazy view as often
-vim.keymap.del("n", "<Leader>l")
+local normal_map_keys = vim.api.nvim_get_keymap("n")
+for i, v in pairs(normal_map_keys) do
+  if v["lhs"] == " l" then
+    vim.keymap.del("n", "<Leader>l")
+    break
+  end
+end
+
 map("n", "<Leader>ll", "<CMD>Lazy<CR>")
 map("n", "<Leader>lx", "<CMD>LazyExtras<CR>")
 
@@ -32,3 +61,10 @@ map({ "n" }, "<Leader>ci", function()
   local cmp = require("blink.cmp.config.keymap")
   print(vim.inspect(cmp))
 end, { desc = "Ollama complete" })
+
+-- Taken from `Advanced telescope.nvim` Examples, searhc plugins
+map({ "n" }, "<Leader>fp", function()
+  require("telescope.builtin").find_files({
+    cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy"),
+  })
+end, { desc = "Find plugin files via telescope" })
