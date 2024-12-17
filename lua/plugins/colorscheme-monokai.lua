3ad8b559337737b14a057c676e3cfe80ec9dd7c4
Andrei Litvin <andreilitvin@google.com>
2024-12-17 12:29:03 -0500

    Ensure we have a treesitter before monokai, to have nice colors

--- lua/plugins/colorscheme-monokai.lua
+++ lua/plugins/colorscheme-monokai.lua
@@ -1,8 +1,23 @@
 return {
   {
     "loctvl842/monokai-pro.nvim",
+    dependencies = {
+      "nvim-treesitter",
+    },
     config = function()
-      require("monokai-pro").setup()
+      require("monokai-pro").setup({
+        plugins = {
+          bufferline = {
+
+            underline_selected = false,
+            underline_visible = false,
+          },
+          indent_blankline = {
+            context_highlight = "pro", -- default | pro
+            context_start_underline = false,
+          },
+        },
+      })
     end,
   },
   {
