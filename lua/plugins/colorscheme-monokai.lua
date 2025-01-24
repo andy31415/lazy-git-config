ac85bf06876b7e700a7b4fb2d471788bbda79c08
Andrei Litvin <andreilitvin@google.com>
2025-01-24 10:42:17 -0500

    Various fixes

--- lua/plugins/colorscheme-monokai.lua
+++ lua/plugins/colorscheme-monokai.lua
@@ -8,7 +8,6 @@ return {
       require("monokai-pro").setup({
         plugins = {
           bufferline = {
-
             underline_selected = false,
             underline_visible = false,
           },
@@ -17,6 +16,25 @@ return {
             context_start_underline = false,
           },
         },
+        override = function()
+          -- I find black not that visisble
+          return {
+            WinSeparator = { fg = "#821803" },
+          }
+        end,
+        ---@param cs Colorscheme
+        ---@param p ColorschemeOptions
+        ---@param config MonokaiProOptions
+        ---@param hp Helper
+        overrideScheme = function(cs, p, config, hp)
+          local cs_override = {}
+          local calc_bg = hp.blend(p.background, 0.85, "#000000")
+
+          cs_override.editor = {
+            background = calc_bg,
+          }
+          return cs_override
+        end,
       })
     end,
   },
